f04c5d8a067de8cc1ee0e61e4066f882dee840d2
aik <aik.fiend@gmail.com>
2021-05-25 14:41:36 +0300

    Moves test data to a separate class, adds exceptions throwing to date validation to be called before all tests

--- datepicker-project/src/test/java/DatepickerTest.java
+++ datepicker-project/src/test/java/DatepickerTest.java
@@ -1,17 +1,14 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import utils.TestUtils;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
-
-public class DatepickerTests {
-
-    String URL = "https://demoqa.com/automation-practice-form",
-            dayOfBirth = "29",
-            monthOfBirth = "November",
-            yearOfBirth = "1922";
-
-    String dateOfBirth = monthOfBirth.concat(" ").concat(dayOfBirth).concat(", ").concat(yearOfBirth);
+public class DatepickerTest extends TestData {
+    @BeforeAll
+    public static void chkDate() throws TestUtils.InvalidDateException {
+        TestUtils.isValidDate(dateOfBirth);
+    }
 
     @Test
     void datepickerSelenideElementsCollectionTest() {
@@ -22,17 +19,14 @@ public class DatepickerTests {
         $(".react-datepicker__year-select").selectOption(yearOfBirth);
         $$(".react-datepicker__day:not(.react-datepicker__day--outside-month)").findBy(text(dayOfBirth)).click();
 
-/*      Assertion:
-        $(".modal-body tbody tr", 4)
+/*        $(".modal-body tbody tr", 4)
                 .shouldHave(text("Date of Birth"))
                 .shouldHave(text(dayOfBirth + " " + monthOfBirth + "," + yearOfBirth));
- */
+*/
     }
 
     @Test
     void datepickerSelenideElementTest() {
-        TestUtils.isValidDate(dateOfBirth);
-
         open(URL);
 
         $("#dateOfBirthInput").click();
@@ -40,10 +34,9 @@ public class DatepickerTests {
         $(".react-datepicker__year-select").selectOption(yearOfBirth);
         $(".react-datepicker__day[aria-label*='" + TestUtils.ordinalDayDateFormat(dateOfBirth) + "']").click();
 
-/*      Assertion:
-        $(".modal-body tbody tr", 4)
+/*        $(".modal-body tbody tr", 4)
                 .shouldHave(text("Date of Birth"))
                 .shouldHave(text(dayOfBirth + " " + monthOfBirth + "," + yearOfBirth));
- */
+*/
     }
 }
--- datepicker-project/src/test/java/utils/TestUtils.java
+++ datepicker-project/src/test/java/utils/TestUtils.java
@@ -5,20 +5,33 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class TestUtils {
-    public static void isValidDate(String dateStr) {
-        DateFormat sdf = new SimpleDateFormat("MMM dd, yyyy");
-        sdf.setLenient(false);
-        try {
-            sdf.parse(dateStr);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-    }
-
     public static String ordinalDayDateFormat(String dateStr) {
         return dateStr.replaceAll("(\\s[23]*1)(,\\s)", "$1st$2")
                 .replaceAll("(\\s[2]*2)(,\\s)","$1nd$2")
                 .replaceAll("(\\s[2]*3)(,\\s)", "$1rd$2")
                 .replaceAll("([2]*[4-9]|[10-20]|30)(,\\s)", "$1th$2");
+    }
+
+    /**
+     * @throws InvalidDateException when date is wrong or date format is not valid
+    */
+    public static boolean isValidDate(String dateStr) throws InvalidDateException {
+        DateFormat sdf = new SimpleDateFormat("MMM dd, yyyy");
+        sdf.setLenient(false);
+        try {
+            sdf.parse(dateStr);
+        } catch (ParseException ex) {
+            throw new InvalidDateException(
+                    "Wrong date or date format is not valid, the date should be in format: MMM dd, YYYY",
+                    ex
+            );
+        }
+        return true;
+    }
+
+    public final static class InvalidDateException extends Exception {
+        public InvalidDateException (final String msg, final Throwable cause) {
+            super (msg, cause);
+        }
     }
 }
